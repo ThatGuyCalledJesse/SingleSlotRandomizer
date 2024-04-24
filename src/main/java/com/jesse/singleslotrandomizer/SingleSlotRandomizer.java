794a2f22cfc71c420aa5d2d7256010c97328e037
Jesse Swart <jesseswart08@gmail.com>
2024-04-24 19:17:27 +0200

    Wrote a welcome message

--- src/main/java/com/jesse/singleslotrandomizer/SingleSlotRandomizer.java
+++ src/main/java/com/jesse/singleslotrandomizer/SingleSlotRandomizer.java
@@ -1,17 +1,12 @@
 package com.jesse.singleslotrandomizer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SingleSlotRandomizer extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getServer().broadcastMessage("[SSR] SingleSlotRandomizer is enabled!");
     }
 }
